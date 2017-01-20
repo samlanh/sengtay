7ca35184212c5adef71e628d8dcf2aa069df2254
samlanh <mok_channy@yahoo.com>
2017-01-20 21:48:12 +0700

    update

--- admin/slide/add.php
+++ admin/slide/add.php
@@ -2,9 +2,90 @@
 include "init.php";
 ?>
 <div id="page-wrapper" style="padding: 15px; width: 1080px;" >
+    
+    <?php
+    
+        if (isset($_POST['btnaddslide'])){
+            $txttitle=$_POST['txttitle'];
+            $orderpost=$_POST['orderpost'];
+
+            $imagesfile1=$_FILES['user_image']['name'];
+            $images_dir1=$_FILES['user_image']['tmp_name'];
+            $imagSize1=$_FILES['user_image']['size'];
+
+            $errorPost=array();
+
+            if (empty($txttitle)){
+                $errorPost[] = '<div class="alert alert-danger alert-dismissable fade in" style="font-size: 15px;margin: 10px;">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                        Please input title
+                    </div>';
+            }
+            if (empty($imagesfile1)){
+                $errorPost[] = '<div class="alert alert-danger alert-dismissable fade in" style="font-size: 15px;margin: 10px;">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                        Please choose your image slide
+                    </div>';
+            }
+            foreach ($errorPost as $errorShow){
+                echo $errorShow;
+            }
+
+            if (empty($errorShow)){
+
+                $upload_dir1="../img/slide/";
+
+                $imgExt1=strtolower(pathinfo($imagesfile1,PATHINFO_EXTENSION));
+                $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
+                $userPostImg=rand(1000,1000000).".".$imgExt1;
+                if (in_array($imgExt1,$valid_extensions)){
+                    if ($imagSize1<5000000){
+                        move_uploaded_file($images_dir1,$upload_dir1.$userPostImg);
+                    }else{
+                        $errMSG = "Sorry, your file is too large.";
+                    }
+                }else{
+                    $errMSG = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                }
+                if (!isset($errMSG)){
+
+                    $stmt=$con->prepare("INSERT INTO tbl_slide (slide_tile,slide_image,orderSlide)
+
+          VALUES (:zslide_tile,:zslide_image,:zorderSlide)
+        ");
+                    $insert= $stmt->execute(array(
+                       'zslide_tile'=>$txttitle,'zslide_image'=>$userPostImg,'zorderSlide'=>$orderpost
+                    ));
+                    if ($insert){
+                        echo ' <div class="alert alert-success alert-dismissable fade in" style="margin: 0 25px;">
+                                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                    <strong>Success!</strong> '.$txttitle.' save success
+                                </div>
+                                <hr>';
+                    }else{
+                        echo '
+                                                <div class="alert alert-danger alert-dismissable fade in" style="margin: 0 25px;">
+                                                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                                    <strong>Fail!</strong> '.$title.' save fail
+                                                </div>
+                        ';
+                    }
+
+                }else{
+                    echo "Error";
+                }
+
+
+
+            }
+
+        }
+    
+    ?>
+    
     <!--class row-->
     <div class="row">
-        <form method="POST">
+        <form method="POST" enctype="multipart/form-data">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <div class="row" style="padding: 10px;">
@@ -14,7 +95,7 @@ include "init.php";
                         <div class="col col-sm-6">
 
                             <div class="pull-right">
-                                <button  name="btnMenu" class=" btn btn-primary"><i class="fa fa-pencil"></i>  Save</button>
+                                <button  name="btnaddslide" class=" btn btn-primary"><i class="fa fa-pencil"></i>  Save</button>
                                 <a href="slidemanage.html" class="btn btn-danger "><i class="fa fa-backward"></i> back</a>
                             </div>
                         </div>
@@ -27,7 +108,7 @@ include "init.php";
                                 <label class="pull-right">Title</label>
                             </div>
                             <div class="col col-sm-10">
-                                <input class="form form-control" type="text" name="txtMenu" placeholder="Input slide name">
+                                <input class="form form-control" type="text" name="txttitle" placeholder="Input slide name">
                             </div>
                             
 
@@ -37,26 +118,30 @@ include "init.php";
                             <div class="form-group">
                                 <label class="control-label col-sm-4">Order</label>
                                 <div class="col-sm-8">
-                                    <select class="form-control" name="status">
+                                    <select class="form-control" name="orderpost">
                                         <option value="1" selected>First slide</option>
                                         <option value="2">Second slide</option>
-                                        <option value="2">Third slide</option>
-                                        <option value="2">Last slide</option>
+                                        <option value="3">Third slide</option>
+                                        <option value="4">Last slide</option>
 
                                     </select>
                                 </div>
 
                             </div>
-
-
                         </div>
                     <div class="col col-sm-3">
-                        <input type="file" id="uploadslide">
-                        </div>
+
+                        <input id="uploadslide"  class="input-group" type="file" name="user_image" accept="image/*" />
+                    </div>
                     <div class="col col-sm-12"><br>
                         <div class="slide_backend">
+                            
+                            <img src="../img/logo/slide.png" style="height: 340px; margin-left: 300px;">
 
-                            <img src="../img/logo/slide.png" style="height: 340px;margin-left: 300px ">
+                            <div class="info">
+                                <h3>Recommend width 1024X340</h3>
+                                
+                            </div>
 
                         </div>
                     </div>
--- admin/slide/manage.php
+++ admin/slide/manage.php
@@ -1,1 +1,183 @@
 <?php
+include "init.php";
+?>
+ <!-- Page Content -->
+    <div id="page-wrapper">
+
+    <br>
+            <form method="post">
+
+                                <!-- /.row -->
+                                <div class="row">
+                                        <div class="col-lg-12">
+                                        <?php
+
+                                        if (isset($_GET['disableid'])){
+
+                                            $status=0;
+
+                                            $disableid=$_GET['disableid'];
+                                            $stmtUp=$con->prepare("UPDATE tbl_article SET status=? WHERE artcle_id=?");
+                                            $result=$stmtUp->execute(array($status,$disableid));
+                                            if ($result){
+                                                echo "<div class='alert alert-success'>You unblock successfull </div>";
+                                            }
+                                        }
+                                        if (isset($_GET['publicid'])){
+                                            $status=1;
+                                            $publicid=$_GET['publicid'];
+                                            $stmtUp1=$con->prepare("UPDATE tbl_article SET status=? WHERE artcle_id=?");
+                                            $result=$stmtUp1->execute(array($status,$publicid));
+                                            if ($result){
+                                                echo "<div class='alert alert-success'>You public successfull </div>";
+                                            }
+
+                                        }
+
+                                        ?>
+
+                                                    <div class="panel panel-default">
+                                                    <div class="panel-heading">
+                                                       <h3 style="color: #428bca;">Manage article</h3>
+
+
+                                                        <div class="pull-right">
+                                                            <a href="categoryaddcat" class="btn btn-primary" style="margin-top: -70px;"><i class="fa fa-plus"> Create Category </i></a>
+                                                        </div>
+                                                    </div>
+                                                    <!-- /.panel-heading -->
+                                                    <div class="panel-body">
+                                                    <div class="table-responsive">
+                                                        <table class="table table-striped table-hover" id="dataTables-example">
+                                                            <thead>
+                                                            <tr>
+                                                                <th>  <button name="deleteSelect" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> </button></th>
+
+                                                                <th>No</th>
+                                                                <th>IMG</th>
+                                                                <th>Category</th>
+                                                                <th>Category </th>
+                                                                <th>New price</th>
+                                                                <th>Control</th>
+
+                                                            </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                            <?php
+
+
+                                                            $stmt=$con->prepare("
+                                                                      
+                                                                      SELECT tbl_article.*,tbl_category.category as category FROM tbl_article INNER JOIN tbl_category
+                                                                      ON tbl_article.category_id=tbl_category.category_id  WHERE tbl_article.trust='1' ORDER BY article DESC
+                                                                      
+                                  
+                                                              ");
+
+
+                                                         /*   $stmt=$con->prepare("SELECT  tbl_article.*,tbl_menu.menu as menu FROM tbl_article INNER JOIN tbl_menu
+                                                                                ON tbl_article.artcle_id=tbl_menu.menu_id
+
+                                                                              ORDER BY article DESC ");*/
+                                                            $stmt->execute();
+                                                            $rows=$stmt->fetchAll();
+                                                            $i='';
+                                                            foreach ($rows as  $row){
+                                                                $i=$i+1;
+
+                                                                echo "<tr class='odd gradeX'>";
+
+                                                                echo"<td><input type='checkbox' name='del[]' value='".$row['artcle_id']."' ></td>";
+                                                                echo "<td>". $i."</td>";
+                                                              ?>
+                                                                <td style="width:50px;"><img src="../img/<?php echo $row['images_pro']?>" style="max-width: 20px; max-height: 18px;" ></td>
+                                                                <?php
+                                                                echo "<td><a title='Click to update Article' href='article.php?do=edit&id=".$row['artcle_id']."'>".$row['article']."</a></td>";
+                                                                echo "<td class='tblUserTr'>".$row['category']."</td>";
+                                                             echo "<td class='tblUserTr'>".$row['new_price']."$</td>";
+
+                                                                echo "<td class='center-block'>";
+                                                                   if ($row['status']==1){
+                                                                   echo "<a title='Click to disable this article' href='article.php?do=manage&disableid=".$row['artcle_id']."' class='btn btn-info btn-xs'><i class='fa fa-toggle-up' ></i> Public </a>";
+                                                                }
+                                                                if ($row['status']==0){
+                                                                    echo "<a  title='Click to public article' href='article.php?do=manage&publicid=".$row['artcle_id']."' class='btn btn-warning btn-xs'> <i class='fa fa-lock' ></i> Disable</a>";
+                                                                }
+                                                                echo" </td>";
+                                                                echo "</tr>";
+                                                            }
+                                                            ?>
+
+                                                            </tr>
+
+
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+
+                                    <?php
+                                    if (isset($_GET['id']))
+                                        ?>
+
+                                        <!-- /.table-responsive -->
+                                        <!--<div class="well">
+                                            <h4>DataTables Usage Information</h4>
+                                            <p>DataTables is a very flexible, advanced tables plugin for jQuery. In SB Admin, we are using a specialized version of DataTables built for Bootstrap 3. We have also customized the table headings to use Font Awesome icons in place of images. For complete documentation on DataTables, visit their website at <a target="_blank" href="https://datatables.net/">https://datatables.net/</a>.</p>
+                                            <a class="btn btn-default btn-lg btn-block" target="_blank" href="https://datatables.net/">View DataTables Documentation</a>
+                                        </div>-->
+                                        </div>
+                                        <!-- /.panel-body -->
+                                        </div>
+                                        <!-- /.panel -->
+                                        </div>
+                                        <!-- /.col-lg-12 -->
+                                </div>
+                                <!-- /.row -->
+
+
+
+
+
+
+
+
+
+                </form>
+
+    <?php
+
+if (isset($_POST['deleteSelect'])) {
+
+
+
+    if (empty($_POST['del'])){
+        echo "<div class='alert alert-danger'>Please choose your row <strong> Checked box</strong> you want to delete</div>";
+    }else{
+
+        $countDel = $_POST['del'];
+        $n = count($countDel);
+        for ($i = 0; $i < $n; $i++) {
+            $stmt = $con->prepare("DELETE FROM 	tbl_menu WHERE menu_id = :mid");
+            $stmt->bindParam(':mid', $countDel[$i]);
+            $stmt->execute();
+
+            echo $countDel[$i];
+
+        }
+    }?>
+    <script type="text/javascript">
+        window.location.href=window.location.href
+    </script>
+    <?php
+
+}
+    ?>
+
+
+
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php
+include "include/tpls/footer.php";
+
