34f6761c60b373f9e598eacdf9be8eef145eea48
samlanh <mok_channy@yahoo.com>
2017-01-23 12:34:00 +0700

    sart bb

--- index.php
+++ index.php
@@ -6,7 +6,64 @@ include"init.php";
 <div class="contaner">
  <div class="specials">
   <nav class="category">
-   <li class="st_ca"><h6>Specials &nbsp<span class="head_spr"></span></h6></li>
+   <li class="st_ca"><h6>Specials &nbsp</h6></li>
+   <li class="hr_head"><li>
+  </nav>
+
+ <div id="specials">
+	 <?php
+
+	 	include "admin/connection.php";
+
+	 	$stmt=$con->prepare("SELECT * FROM tbl_article WHERE 	promotion='1' and trust='1'");
+	 	$stmt->execute();
+	 	$spacial=$stmt->fetchAll();
+
+	 foreach ($spacial as $rowSpacial) {
+		 ?>
+
+		 <div class="tb_row">
+			 <img src="img/<?php echo $rowSpacial['images_pro'];?>" style=" position: absolute;left: 25px;" width="200px"  height="150px">
+			 <div class="priceShow"><span><del/>$ <?php echo $rowSpacial['old_price'];?></span> - $<?php echo $rowSpacial['new_price'];?></div>
+			 <div class="tilePro">
+				 <h3><?php echo $rowSpacial['article'];?></h3>
+			 </div>
+
+			 <?php
+			 $arrival=$rowSpacial['arrival_comming'];
+			 if ($arrival==1){
+				echo '<div class="newArrival">
+				 <img src="img/STCComputer/newarrival.gif">
+			 </div>';
+			 }else if ($arrival==0){
+
+				 echo '<div class="newArrival" style="color: red;">
+				 New arrival
+			 </div>';
+			 }
+
+			 ?>
+			 <div class="descripshow">
+				 <h5>
+					 <?php echo $rowSpacial['descrip'];?>
+				 </h5>
+			 </div>
+
+
+		 </div>
+
+	 <?php
+	 	}
+	 ?>
+
+
+
+ </div>
+</div>
+
+ <div class="specials_2">
+  <nav class="category">
+   <li class="st_ca"><h6>Specials &nbsp</h6></li>
    <li class="hr_head"><li>
   </nav>
 
@@ -61,7 +118,6 @@ include"init.php";
  </div>
 </div>
 </div>
-
 
 
 <?php
