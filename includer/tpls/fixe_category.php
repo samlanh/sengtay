ff4f1772d3fa5472f69fcc3e4a6bbe1bf81423d4
samlanh <mok_channy@yahoo.com>
2017-01-23 13:53:57 +0700

    hh

--- includer/tpls/fixe_category.php
+++ includer/tpls/fixe_category.php
@@ -15,5 +15,6 @@
 		<lo class="fb_book"><a target="_blank" href="https://www.facebook.com/sengtaycomputer"><img src="img/STCcomputer/facebook.png" width="40"></a></lo>
 		<lo class="fb_youtube"><a target="_blank" href="https://youtube.com"><img src="img/unnamed.png" width="55"></a></lo>
 		<lo class="fb_job"><a href="job_opportunity.php"><img src="img/job.png" width="100%" ></a></lo>
+		<lo class="fb_cunter"><a href="http://s11.flagcounter.com/more/AzM0"><img src="http://s11.flagcounter.com/count2/AzM0/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a><lo/>
     </nav>
 </div>
