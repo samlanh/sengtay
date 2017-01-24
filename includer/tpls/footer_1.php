09682a48275f60fa4ec49ddbf5dc2faab5837924
samlanh <mok_channy@yahoo.com>
2017-01-24 08:19:30 +0700

    asdfa

--- includer/tpls/footer_1.php
+++ includer/tpls/footer_1.php
@@ -1,7 +1,42 @@
 <div style="color:#fff">.</div>
 <div class="footer">
 	<div style="text-align: center;" class="con_fo">
-		<div class="Information">
+
+		<?php
+		include "admin/connection.php";
+		$stmt=$con->prepare("SELECT * FROM tbl_footer_cat WHERE trust='1' and status='1'");
+		$stmt->execute();
+		$rows=$stmt->fetchAll();
+		$rowCount=$stmt->rowCount();
+
+		foreach ($rows as $row){
+		echo $idcatfooter=$row['footer_cat_id'];
+			?>
+					<div class="Information" style="width:<?php echo 100/$rowCount.'%';?> ">
+						<h2><b/><?php echo $row['footer_title']; ?></h2>
+
+						<?php
+						$stmt=$con->prepare("SELECT * FROM tbl_link_footer WHERE footer_cat_id='$idcatfooter' AND trust='1' and status='1'");
+						$stmt->execute();
+						$rows1=$stmt->fetchAll();
+						foreach ($rows1 as $row1){
+							
+						echo '<a href="about.php?linkid='.$row1['link_footer_id'].'"><h5>'.$row1['linkFooter'].'</h5></a>';
+						}
+
+						?>
+
+
+
+
+
+					</div>
+			<?php
+		}
+
+		?>
+
+		<!--<div class="Information">
 			<h2><b/>Information</h2>
 			<a href="about.php"><h5 style="margin-left:-26px">About Us</h5></a>
 			<a href="job_opportunity.php"><h5 style="margin-left:4px">Job Opportunity</h5></a>
@@ -16,7 +51,7 @@
 			<h2><b/>Extras</h2>
 			<a href=""><h5>Brands</h5></a>
 			<a href="Specials_offers.php"><h5>Specials</h5></a>
-		</div>
+		</div>-->
 		
 
 	</div>
