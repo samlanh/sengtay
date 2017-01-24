7b17b4ba9fb76c247bd0d612e78cfb304382bb09
samlanh <mok_channy@yahoo.com>
2017-01-24 08:20:22 +0700

    dfdsfadafdadf

--- includer/tpls/about.php
+++
@@ -1,7 +0,0 @@
-
-<div class="about">
-	<div class="cat_g">
-		<h2>About Us</h2>
-		<p>about us <p>
-	</div>
-</div>
--- includer/tpls/fixe_category.php
+++ includer/tpls/fixe_category.php
@@ -5,7 +5,7 @@
 		
 		<?php
 	include "admin/connection.php";
-		$stmt=$con->prepare("SELECT * FROM tbl_category WHERE tbl_category.trust='1'");
+		$stmt=$con->prepare("SELECT * FROM tbl_category WHERE tbl_category.trust='1' and menu_id='26'");
 		$stmt->execute();
 		$spacial=$stmt->fetchAll();
 		foreach ($spacial as $spacailcat){
@@ -26,9 +26,10 @@
 		<lo><a title="Acer" href="#acer"><img src="img/STCcomputer/acer.jpg"></a></lo>
 		<lo><a title="MSI" href="#msi"><img src="img/STCcomputer/msi.png"></a> </lo>
 		<lo><a title="Microsoft Surface" href="#microsoft"><img src="img/STCcomputer/surface.jpg"></a></lo>
+		-->
 		<lo class="fb_book"><a target="_blank" href="https://www.facebook.com/sengtaycomputer"><img src="img/STCcomputer/facebook.png" width="40"></a></lo>
 		<lo class="fb_youtube"><a target="_blank" href="https://youtube.com"><img src="img/unnamed.png" width="55"></a></lo>
 		<lo class="fb_job"><a href="job_opportunity.php"><img src="img/job.png" width="100%" ></a></lo>
 		<lo class="fb_cunter"><a href="http://s11.flagcounter.com/more/AzM0"><img src="http://s11.flagcounter.com/count2/AzM0/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a><lo/>
--->    </nav>
+    </nav>
 </div>
